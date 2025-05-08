27afccd96c835b25936623682578eea24594d798
AcctAsmaa <asom022@gmail.com>
2025-05-08 23:25:02 +0300

    edit home page

--- home.js
+++ home.js
@@ -2,12 +2,13 @@ const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
 btn1.addEventListener('click', () => {
-  location.replace('intermediate.html');
+  location.replace('exam.html');
 
 });
 
 btn2.addEventListener('click', () => {
-  location.replace('exam.html');
+  location.replace('intermediate.html');
+
 
 });
 
